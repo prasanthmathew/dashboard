27bce30fdf2baf02b6410bae1c95967f356e8993
Prasanth <prasanthvadakkel@gmail.com>
2014-08-02 12:33:06 +0530

    Changed Login template

--- application/modules/templates/views/login_template_view.php
+++ application/modules/templates/views/login_template_view.php
@@ -24,14 +24,14 @@
     </head>
     <body>
         <section id="content" class="m-t-lg wrapper-md animated fadeInUp">
-            <a class="nav-brand" href="index.html">todo</a>
+            <a class="nav-brand" href="<?php echo site_url(); ?>">Dashboard</a>
             <div class="row m-n">
                 <div class="col-md-4 col-md-offset-4 m-t-lg">
                     <section class="panel">
                         <header class="panel-heading text-center">
                             Sign in
                         </header>
-                        <form action="index.html" class="panel-body">
+                        <form action="<?php echo site_url(); ?>" method="post" class="panel-body">
                             <div class="form-group">
                                 <label class="control-label">Email</label>
                                 <input type="email" placeholder="test@example.com" class="form-control">
@@ -45,14 +45,12 @@
                                     <input type="checkbox"> Keep me logged in
                                 </label>
                             </div>
-                            <a href="#" class="pull-right m-t-xs"><small>Forgot password?</small></a>
+                            <a href="<?php echo site_url(); ?>" class="pull-right m-t-xs"><small>Forgot password?</small></a>
                             <button type="submit" class="btn btn-info">Sign in</button>
-                            <div class="line line-dashed"></div>
-                            <a href="#" class="btn btn-facebook btn-block m-b-sm"><i class="fa fa-facebook pull-left"></i>Sign in with Facebook</a>
-                            <a href="#" class="btn btn-twitter btn-block"><i class="fa fa-twitter pull-left"></i>Sign in with Twitter</a>
+                            <div class="line line-dashed"></div>                            
                             <div class="line line-dashed"></div>
                             <p class="text-muted text-center"><small>Do not have an account?</small></p>
-                            <a href="signup.html" class="btn btn-white btn-block">Create an account</a>
+                            <a href="<?php echo site_url(); ?>" class="btn btn-white btn-block">Create an account</a>
                         </form>
                     </section>
                 </div>
@@ -62,17 +60,17 @@
         <footer id="footer">
             <div class="text-center padder clearfix">
                 <p>
-                    <small>Mobile first web app framework base on Bootstrap<br>&copy; 2013</small>
+                    <small><?php echo $this->config->item('COPY_RIGHT'); ?></small>
                 </p>
             </div>
         </footer>
         <!-- / footer -->
-        <script src="js/jquery.min.js"></script>
+        <script src="<?php echo base_url($this->config->item('BACKEND_ASSETS')); ?>/js/jquery.min.js"></script>
         <!-- Bootstrap -->
-        <script src="js/bootstrap.js"></script>
+        <script src="<?php echo base_url($this->config->item('BACKEND_ASSETS')); ?>/js/bootstrap.js"></script>
         <!-- app -->
-        <script src="js/app.js"></script>
-        <script src="js/app.plugin.js"></script>
-        <script src="js/app.data.js"></script>
+        <script src="<?php echo base_url($this->config->item('BACKEND_ASSETS')); ?>/js/app.js"></script>
+        <script src="<?php echo base_url($this->config->item('BACKEND_ASSETS')); ?>/js/app.plugin.js"></script>
+        <script src="<?php echo base_url($this->config->item('BACKEND_ASSETS')); ?>/js/app.data.js"></script>
     </body>
 </html>
